d1d69c942c9514a750d83aaa595e59a0295a52f9
JuuanmaSR <juan_rider_sp@hotmail.com>
2022-09-21 14:12:57 -0300

    Se implemento funcionalidad de login
    - Se implemento esta funcionalidad para que los usuarios logeados puedan  guardar gifs favoritos.
    -

---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "wouter"
+import useUser from "hooks/useUser"
+import './index.css'
+
+const Header = () => {
+    //const isLogged = false
+    const { isLogged, logout } = useUser()
+
+    const handleClick = () => {
+        logout()
+    }
+
+    return (
+        <header className="giffy-header">
+            {
+                isLogged ? <button className="nv-logout-button" onClick={handleClick}>Logout</button> :
+                    <Link to="/login">Logín</Link>
+            }
+        </header>
+    )
+}
+
+
+export default Header
