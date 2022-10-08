cf370baa26ba37f4dd6726b2916c85aa2c5daa05
JuuanmaSR <juan_rider_sp@hotmail.com>
2022-10-08 13:55:23 -0300

    Se implementa componente
    - Se implementa el componente de Login para poder gestionar favoritos.
    - Se saca la funcionalidad de Pages y se pasa a un Componente reutilizable.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,51 +1,11 @@
-import useUser from "hooks/useUser"
-import React, { useEffect, useState } from "react"
-import { useLocation } from "wouter"
-import './index.css'
-const Login = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [_, navigate] = useLocation()
-    const { login, isLogged, isLoginLoading, hasLoginError } = useUser()
+import React from "react";
+import Login from "components/Login/Login";
 
-    useEffect(() => {
-        if (isLogged) {
-            navigate('/')
-        }
-    }, [isLogged, navigate])
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        login({ username, password })
-        //navigate('/')
-    }
-
-    const handleUsernameChange = evt => {
-        setUsername(evt.target.value)
-    }
-
-    const handlePasswordChange = evt => {
-        setPassword(evt.target.value)
-    }
-    return (
-        <>
-            <h2>Login</h2>
-            {isLoginLoading && <strong>Checking credentials...</strong>}
-            {!isLoginLoading &&
-                <form onSubmit={handleSubmit} className='loginForm'>
-                    <input placeholder="username" value={username} onChange={handleUsernameChange} />
-                    <input placeholder="password" type='password' value={password} onChange={handlePasswordChange} />
-                    <button>Logín</button>
-
-                </form>
-            }
-            {hasLoginError &&
-                <strong>Credentials are invalid</strong>
-            }
-
-        </>
-    )
+const LoginPage = () => {
+    return <>
+        <h2>Login</h2>
+        <Login />
+    </>
 }
 
-
-export default Login
+export default LoginPage
