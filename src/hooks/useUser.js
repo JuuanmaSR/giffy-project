92a65a03e25a0a853ba32fa7e71deed558d2fc12
JuuanmaSR <juan_rider_sp@hotmail.com>
2022-10-08 19:41:59 -0300

    Implementando manejo de errores
    - Se intenta manejar errores del formulario de registro para mostrarlos por pantalla.

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,28 +1,40 @@
 import { useContext, useCallback, useState } from "react"
 import Context from "context/UserContext"
 import loginService from "services/login"
+import registerService from "services/register"
 import addFavService from 'services/addFavs'
 import deleteFavService from "services/deleteFavs"
 
 const useUser = () => {
     const { favs, jwt, setFavs, setJWT } = useContext(Context)
-    const [state, setState] = useState({ loading: false, error: false })
+    const [loginState, setLoginState] = useState({ loading: false, error: false })
+    const [registerState, setRegisterState] = useState({ loading: false, error: false, isRegistered: false })
 
     const login = useCallback(({ username, password }) => {
-        setState({ loading: true, error: false })
+        setLoginState({ loading: true, error: false })
         loginService({ username, password })
             .then(jwt => {
                 window.sessionStorage.setItem('jwt', jwt)
-                setState({ loading: false, error: false })
+                setLoginState({ loading: false, error: false })
                 setJWT(jwt)
             })
             .catch(err => {
                 window.sessionStorage.removeItem('jwt')
-                setState({ loading: false, error: true })
+                setLoginState({ loading: false, error: err })
                 console.error(err)
             })
     }, [setJWT])
 
+    const register = ({ username, password }) => {
+        setRegisterState({ loading: true, error: false })
+        registerService({ username, password })
+            .then(() => {
+                setRegisterState({ loading: false, error: false, isRegistered: true })
+            }).catch(err => {
+                setRegisterState({ loading: false, error: true, isRegistered: false })
+                console.error(err)
+            })
+    }
     const addFav = useCallback(({ id }) => {
         addFavService({ id, jwt })
             .then(favs => setFavs(favs))
@@ -46,9 +58,13 @@ const useUser = () => {
 
     return {
         isLogged: Boolean(jwt),
-        isLoginLoading: state.loading,
-        hasLoginError: state.error,
+        isLoginLoading: loginState.loading,
+        hasLoginError: loginState.error,
+        isRegistered: registerState.isRegistered,
+        isRegisterLoading: registerState.loading,
+        hasRegisterError: registerState.error,
         login,
+        register,
         logout,
         addFav,
         deleteFav,
