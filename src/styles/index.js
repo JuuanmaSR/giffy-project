c6d305705bc3aeedda08800dea33cbb3e670e209
JuuanmaSR <juan_rider_sp@hotmail.com>
2022-10-28 14:10:07 -0300

    Implementando libreria para manejar estilos de manera general

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,3 +1,10 @@
 export const bps = {
     greaterThanMobile: '@media screen and (min-width: 880px)'
 }
+
+export const theme = {
+    colors: {
+        primary: 'var(--theme-primary-color)',
+        secondary: 'var(--theme-secondary-color)'
+    }
+}
