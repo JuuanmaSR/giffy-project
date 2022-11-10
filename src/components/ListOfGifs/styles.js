4608d263b9735966fafc35f60914b199cab5db0c
JuuanmaSR <juan_rider_sp@hotmail.com>
2022-11-10 13:50:05 -0300

    Se migro a styles components
    - Se migro la lista que muestra los gifs para mejorar el rendimiento de los estilos.

--- src/components/ListOfGifs/styles.js
+++ src/components/ListOfGifs/styles.js
@@ -1,7 +1,10 @@
-.ListOfGifs {
+import styled from "@emotion/styled";
+
+
+export const GifContainer = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fit,minmax(200px, 1fr));
     grid-gap: 15px 15px;
     align-items: center;
     min-height: 100vh;
-}
+`
